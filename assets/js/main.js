3a641510ea68137570bce84940dbdb111189243c
shawnkieu <99684295+shawnkieu@users.noreply.github.com>
2022-05-10 01:13:50 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+// ================= SERVICES MODAL ========================
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalClose = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+  modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i)=>{
+  mb.addEventListener('click', () => {
+    modal(i)
+  })
+})
+
+modalClose.forEach((mc)=>{
+  mc.addEventListener('click', () => {
+    modalViews.forEach((mv) =>{
+      mv.classList.remove('active-modal')
+    })
+  })
+});
+
+// =============== MIXITUP FILTER Portfolio ================
+let mixerPortfolio = mixitup('work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+}
+);
+
+// Link active work
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork(){
+  linkWork.forEach(l => l.classList.remove('active-work'))
+  this.classList.add('active-work')
+}
+
+linkWork.forEach(l => l.addEventListener('click',activeWork))
+
+// ================= SWIPER TESTIMONIALS =====================
+
+
+// ================ SCROLL SECTION ACTIVE LINK ===================
